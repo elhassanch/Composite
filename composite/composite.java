c0642d4b36495224dec6cd129250e6a7b89b2f3b
elhassanch <65264383+elhassanch@users.noreply.github.com>
2020-11-07 00:25:05 +0100

    Update composite.java

--- composite/composite.java
+++ composite/composite.java
@@ -5,78 +5,78 @@ import java.util.List;
 
 
 interface Component{
-
-		void afficherInfo();
-		void calculerSize();
+	
+	void afficherInfo();
+	void calculerSize();
 }
 
 
 class Fichier implements Component{
 
-		long size;
-		String name;
+	long size;
+	String name;
 
-		public Fichier(int size, String name) {
+	public Fichier(int size, String name) {
 
-			super();
-			this.size = size;
-			this.name = name;
-
-		}
-
-		public void afficherInfo() {
-
-			System.out.println(“Name:”+name+” Size: ”+size);
-
-			}
-
-		public long calculerSize() {
-
-			return this.size;
-
-		}
+		super();
+		this.size = size;
+		this.name = name;
 
 	}
+
+	public void afficherInfo() {
+
+		System.out.println(“Name:”+name+” Size: ”+size);
+
+	}
+
+	public long calculerSize() {
+
+		return this.size;
+
+	}
+
+}
 
 
 class Repertoire implements Component{	
 
-		String name;
-		List<Component> list=new ArrayList<>();
+	String name;
+	List<Component> list=new ArrayList<>();
 
-		public Repertoire(String name) {
+	public Repertoire(String name) {
 
-			super();
-			this.name = name;
+		super();
+		this.name = name;
+	}
+
+	public void ajouterComposant(Component composant) {
+
+		list.add(composant);
+
+	}
+
+	public void afficherInfo() {
+
+		System.out.println(“Name :”+name);
+
+		for(Component x : list) {
+
+			x.afficherInfo();
+ 			x.calculerSize();
+
+			}
+
+	}
+
+	public long calculerSize() {
+
+		long s=0;
+
+		for(Component com : list){
+			s=s+calculerSize();
 		}
+		return s;
 
-		public void ajouterComposant(Component composant) {
-
-				list.add(composant);
-
-		}
-
-		public void afficherInfo() {
-
-				System.out.println(“Name :”+name);
-
-				for(Component x : list) {
-
-					x.afficherInfo();
- 				    x.calculerSize();
-
-				}
-
-		}
-
-		public long calculerSize() {
-
-				int s=0;
-
-				for(Component com : list){
-					s=s+calculerSize();
-				}
-				return s;
-
-				}
+	}
 }
